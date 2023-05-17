310cd7d02661a05acb7f70410f9fbe36df4364a6
Antonio Cabello Delgado <antonioacd123@gmail.com>
2023-05-17 22:10:37 +0200

    Set img fondo

--- app/src/main/java/com/example/beassistant/controllers/logins/LoginController.java
+++ app/src/main/java/com/example/beassistant/controllers/logins/LoginController.java
@@ -151,7 +151,6 @@ public class LoginController extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
         if (requestCode == 1000){
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
             try {
